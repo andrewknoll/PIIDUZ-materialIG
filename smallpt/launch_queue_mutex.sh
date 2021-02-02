6819abcd2f25eef6a367185a925d51074f909e03
RubÃen Gran Tejero <rgran@unizar.es>
2021-02-02 18:09:24 +0100

    minor changes

--- smallpt/launch_queue_mutex.sh
+++ smallpt/launch_queue_mutex.sh
@@ -2,8 +2,6 @@
 width=4096
 height=2160
 
-mkdir -p /tmp/images
-
 echo "mutex, scene, width, height, threads, samples, divisions, time (s)"
 
 #for mutex in def K n s asm_sleep def 
@@ -12,7 +10,7 @@ do
 for scene in forest
 do
     #for threads in 1 4 8 12 16 20 24 28 32 36 40 40 44 48 52 56 60 64 96 192 384 
-    for threads in 8 48
+    for threads in 8 16 32 48 
     do
         for samples in 1 
         do
@@ -22,7 +20,7 @@ do
                 then
                     continue
                 fi
-                output_file="/tmp/images/queue_mutex_${mutex}_${scene}_${width}_${height}_${threads}_${samples}_${divisions}.png"
+                output_file="./images/queue_mutex_${mutex}_${scene}_${width}_${height}_${threads}_${samples}_${divisions}.png"
                 ex_time=$(./smallpt_queue_mutex_${mutex} -scene "${scene}" -width "${width}" -height "${height}" -threads "${threads}" -samples "${samples}" -divisions ${divisions} -output "${output_file}" 2> /dev/null |  awk  '$1 ~ /Execution/ { print substr($3, 1, length($3)-1)}')
                 echo "${mutex}, ${scene}, ${width}, ${height}, ${threads}, ${samples}, ${divisions}, ${ex_time}"
             done
